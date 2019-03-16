5f3b1a1c9651c241ae6179668fa4655103d5aa07
David Rymer <david-rymer@hotmail.co.uk>
2019-03-16 19:00:53 +0000

    health bars working over network

---
+++ Assets/Scripts/Destroy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    public GameObject bulletHit;
+    public PhotonView photonView;
+
+
+    private int timer = 0;
+    public int bulletDelayTicks = 200;
+
+    void Start()
+    {
+        Destroy(gameObject, 2);
+    }
+
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (!other.gameObject.CompareTag("Rocket"))
+        {
+            Debug.Log(other.gameObject.CompareTag("Rocket"));
+            GameObject explosion =
+                PhotonNetwork.Instantiate(bulletHit.name, transform.position, Quaternion.identity, 0);
+        }
+        
+        Debug.Log(other.gameObject.CompareTag("Rocket") + "dddddddddddd");
+
+
+
+        if (photonView.isMine)
+        {
+            PhotonNetwork.Destroy(gameObject);
+        }
+    }
+}
